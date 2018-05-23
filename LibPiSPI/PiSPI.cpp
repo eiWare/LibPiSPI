6b476c578f395f2f184cefe3cd8a952a68df3358
T.E <teiseler85@gmail.com>
2018-05-23 00:00:55 -0400

    Added copyright notice

--- LibPiSPI/PiSPI.cpp
+++ LibPiSPI/PiSPI.cpp
@@ -199,3 +199,22 @@ int PiSPI::GetMode()
 
 	return this->_iMode = mode;
 }
+
+/*
+Copyright (c) 2018 Tobias Eiseler
+
+This file is part of LibPiSPI.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
--- LibPiSPI/PiSPI.h
+++ LibPiSPI/PiSPI.h
@@ -57,3 +57,21 @@ private:
 
 #endif //__PiSPI_H_
 
+/*
+Copyright (c) 2018 Tobias Eiseler
+
+This file is part of LibPiSPI.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
