edc661ca1bde00004597bc6fc47158142a35ffca
T.E <teiseler85@gmail.com>
2018-09-05 10:02:14 -0400

    Updated the test application

--- Test/main.cpp
+++ Test/main.cpp
@@ -7,12 +7,12 @@
 int main(void)
 {
 	uint8_t buffer[10] = { 0 };
-	PiSPI* pSPI = PiSPI::GetInstance(0, 10000000, SPI_MODE_0);
+	PiSPI piSPI(0, 10000000, 0);
 	while (true)
 	{
 		memset(buffer, 0, sizeof(buffer));
 		buffer[0] = 0x80;
-		pSPI->Read(0x80, &buffer[0], sizeof(buffer));
+		piSPI.Read(0x80, &buffer[0], sizeof(buffer));
 		this_thread::sleep_for(chrono::seconds(1));
 	}
 	return 0;
